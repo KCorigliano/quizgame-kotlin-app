77f71ecba6f0d33bbd0117dfa7fe107776f21f57
Daniel Kuba <king_kubaa@icloud.com>
2026-07-09 09:23:29 +0200

    updated ui

--- app/src/main/java/de/syntax_institut/projektwoche1/QuizScreen.kt
+++ app/src/main/java/de/syntax_institut/projektwoche1/QuizScreen.kt
@@ -1,2 +1,33 @@
 package de.syntax_institut.projektwoche1
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun QuizScreen() {
+
+    Column {
+
+        Text("Frage 1 von 10")
+
+        Text("Wie heißt die Hauptstadt von Deutschland?")
+
+        Button(onClick = { }) {
+            Text("Berlin")
+        }
+
+        Button(onClick = { }) {
+            Text("Paris")
+        }
+
+        Button(onClick = { }) {
+            Text("Rom")
+        }
+
+        Button(onClick = { }) {
+            Text("Madrid")
+        }
+    }
+}
